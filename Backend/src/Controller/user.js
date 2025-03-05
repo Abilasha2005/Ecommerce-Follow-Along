4e563f20a0a6da920af1efc4ce7dee8987113b91
Abilasha Manne <Abilashamanne@gmail.com>
2025-03-05 10:59:48 +0530

    done

--- Backend/src/Controller/user.js
+++ Backend/src/Controller/user.js
@@ -60,5 +60,20 @@ userrouter.post("/login", async(req,res)=>{
       });
 })
 
+userrouter.post('/address', async (req, res) => {
+    const { address, city, state, zip, country } = req.body;
+    try {
+        if (!address || !city || !state || !zip || !country) {
+            return res.status(400).json({ message: "Fill all fields" });
+        }
+        const addressDetails = new addressModel({ address, city, state, zip, country });
+        await addressDetails.save();
+        res.status(200).json({ message: "Address added successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
 
-module.exports = userrouter;
+
+module.exports = userrouter;
