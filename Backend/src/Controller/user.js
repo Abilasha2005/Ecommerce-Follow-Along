d385f7df1872bd9d3afcc05ddc6efaeb97c24c19
Abilasha Manne <Abilashamanne@gmail.com>
2025-03-05 11:12:16 +0530

    redone

--- Backend/src/Controller/user.js
+++ Backend/src/Controller/user.js
@@ -60,19 +60,33 @@ userrouter.post("/login", async(req,res)=>{
       });
 })
 
-userrouter.post('/address', async (req, res) => {
-    const { address, city, state, zip, country } = req.body;
-    try {
-        if (!address || !city || !state || !zip || !country) {
-            return res.status(400).json({ message: "Fill all fields" });
-        }
-        const addressDetails = new addressModel({ address, city, state, zip, country });
-        await addressDetails.save();
-        res.status(200).json({ message: "Address added successfully" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
+userrouter.post('/add-address', async (req, res) => {
+  try {
+    const {
+      country,
+      city,
+      address1,
+      address2,
+      zipCode,
+      addressType,
+      email
+    } = req.body;
+    const user = await userModel.findOne({ email: email });
+    const newaddress = {
+      country,
+      city,
+      address1,
+      address2,
+      zipCode,
+      addressType
+    };
+    user.addresses.push(newaddress);
+    await user.save();
+    res.status(200).json({ message: "Address added successfully" });
+  } catch (err) {
+    console.log("error in adding address", err);
+    res.status(500).json({ message: "Error in adding address" });
+  }
 });
 
 
