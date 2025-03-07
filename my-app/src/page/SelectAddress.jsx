0091d229b96918cdd1decd73379136c3e6c2118e
Abilasha Manne <Abilashamanne@gmail.com>
2025-03-07 11:35:48 +0530

    redone

--- my-app/src/page/SelectAddress.jsx
+++ my-app/src/page/SelectAddress.jsx
@@ -1,82 +1,155 @@
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import axios from 'axios';
+const {Router}= require("express");
+const userModel = require("../Model/userModel");
+const { upload } = require("../../multer");
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const userrouter = Router();
+const AsyncError = require('../Middleware/catchAsyncError');
+const auth = require("../Middleware/auth");
+require('dotenv').config({  path:'./src/config/.env'});
+
+const secret = process.env.secretkey;
+
+userrouter.post("/create-user",upload.single('file'),async(req,res)=>{
+    const {name, email, password} = req.body;
+    const userEmail = await userModel.findOne({email});
+    if (userEmail) {
+        return next(new ErrorHandler("User already exists", 400));
+      }
+
+      const filename = req.file.filename ;
+      const fileUrl = path.join(filename);
+   
+    bcrypt.hash(password, 10,async function(err, hash) {
+
+        await userModel.create({
+            name:name,
+            email:email,
+            password:hash,
+            // avatar:fileUrl
+
+        })
+
+        })
 
 
+});
 
-const SelectAddress=()=>{
-    const [addresses, setAddresses] = useState([]);
-    const [selectedAddress, setSelectedAddress] = useState(null);
-    const location = useLocation();
-    const email = location.state.email;
 
-    // useEffect(() => {
-    //     const fetchAddresses = async () => {
-    //     const { data } = await axios.get('http://localhost:3000/get-address', { email:email });
-    //     setAddresses(data.addresses);
-    //     };
-    
-    //     fetchAddresses();
-    // }, []);
-
-    const add = [
-        {
-          country: "USA",
-          city: "New York",
-          address1: "123 Main St",
-          address2: "Apt 4B",
-          zipCode: 10001,
-          addressType: "Home"
-        },
-        {
-          country: "Canada",
-          city: "Toronto",
-          address1: "456 Maple Ave",
-          address2: "Unit 22",
-          zipCode: 1001,
-          addressType: "Work"
-        },
-        {
-          country: "UK",
-          city: "London",
-          address1: "789 Oxford St",
-          address2: "",
-          zipCode: 10025,
-          addressType: "Vacation Home"
-        }
-      ];
-        useEffect(() => {
-            document.getElementsByTagName('body')[0].style.backgroundColor="white";
-            setAddresses(add);
-        }, []);
-      
-    
-    const handleSelectAddress = (address) => {
-        setSelectedAddress(address);
-    };
-    
-    return (
-        <>
-        <h1 className='text-black'>Select Address</h1>
-        <div className='flex flex-col justify-center justify-items-center items-center'>
-        {addresses.map((address,val) => (
-            <div key={val} onClick={() => handleSelectAddress(address)} className='rounded-md text-black' style={{cursor: 'pointer', border: '1px solid black', padding: '10px', margin: '10px',width:'250px',height:'150px'}}>
-            <h2>{address.addressType}</h2>
-            <div>{address.address1},<br /> {address.address2} <br /> {address.city},{address.country} - {address.zipCode} </div>
-
-            </div>
-        ))}
-        {selectedAddress && (
-            <div className='text-black rounded-md' style={{cursor: 'pointer', border: '1px solid black', padding: '10px', margin: '10px',width:'250px',height:'150px'}}>
-            <h2>Selected Address</h2>
-            <div>{selectedAddress.address1},<br /> {selectedAddress.address2} <br /> {selectedAddress.city},{selectedAddress.country} - {selectedAddress.zipCode} </div>
-
-            </div>
-        )}
-        </div>
-        <button className='text-black bg-white border border-black'>Continue</button>
-        </>
-    );
+userrouter.post("/login",async(req,res)=>{
+    const {email, password} = req.body;
+    const check= await userModel.findOne({email:email});
+        console.log(check);
+    if(!check){
+        return res.status(400).json({message:"User not found"});
     }
 
-export default SelectAddress;
+    bcrypt.compare(password,check.password , function(err, result) {
+       
+        if(err){
+            return res.status(400).json({message:"Invalid bcrpyt compare"});
+        }
+        if(result){
+            
+            jwt.sign({email:email},secret,(err,token)=>{
+                if(err){
+                    return res.status(400).json({message:"Invalid jwt"});
+                }
+               res.setHeader("Autherization",Bearer ${token})
+                console.log(token);
+                res.status(200).json({token:token});    
+            })
+
+
+        }   
+        else{
+            return res.status(400).json({message:"Invalid password"});
+        }
+    });
+
+})
+
+
+userrouter.get('/profile',async (req,res) => {
+    const {email} = req.query;
+    if(!email){
+        return res.status(400).json({message:"email cannot be empty!"})
+    }
+
+    try{
+    const user = await userModel.findOne({email:email});
+
+    if(!user){
+        return res.status(404).json({message:"The user was not found"});
+    }
+
+    const Users = {
+        name: user.name,
+        email: user.email,
+        phone: user.phoneNumber,
+        image: user.avatarurl,
+        address: user.address
+    }
+
+    res.status(200).json({message:"successfully recieved"});
+}
+
+catch(err){
+    res.status(500).json("error":err);
+}
+})
+
+
+
+
+userrouter.post('/add-address',auth, async(req,res)=>{
+
+    try{
+
+        const email=req.user
+    const {country,
+        city,
+        address1,
+        address2,   
+        zipCode,
+        addressType}=req.body
+
+        const user=await userModel.find({email:email})
+
+      const newaddress={
+        country,
+        city,
+        address1,
+        address2,
+        zipCode,
+        addressType,
+    }
+
+    user.addresses.push(newaddress)
+    await user.save()
+}
+catch(err){
+    console.log("error in address",err)
+}
+})
+
+userrouter.get('/get-address',auth,async(req,res)=>{
+   const email=req.user
+   try{
+    const user=await userModel.findOne({email:email})
+    if(!user){
+        return res.status(400).json({message:"User not found"})
+    }   
+    res.status(200).json({message:"successfully recieved",user:user.addresses
+    })
+   }
+   catch(err){
+         console.log("error in get address",err)    
+   }
+
+
+
+})
+
+
+module.exports = userrouter;
