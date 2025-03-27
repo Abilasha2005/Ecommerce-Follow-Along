f7c75231ca76d748c4eb6de4e16132c62efd2a3d
Abilasha Manne <Abilashamanne@gmail.com>
2025-03-27 13:11:20 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -1,31 +1,39 @@
-const express=require('express');
+const express = require('express');
 const connectDB = require('./src/Database/db');
-const userRouter = require('./src/Controller/user');
-const productrouter = require('./src/Controller/Products');
+const cors = require('cors');
+const userrouter = require('./src/Controllers/user');
+const productrouter = require('./src/Controllers/Products');
+const profileRouter = require('./src/Controllers/profile');
+const orderRouter = require('./src/Controllers/Order'); 
+const cookieParser = require('cookie-parser');
 
-const app=express();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser())
 
 require('dotenv').config({
-    path:'./src/config/.env'
+    path: './src/config/.env'
 });
 
-const PORT=process.env.port || 5000;
-const url=process.env.db_url;
+const PORT = process.env.port || 5000;
+const url = process.env.db_url;
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-})
-
-
-app.use('/auth', userRouter);
-app.listen(PORT,async()=>{
-app.use("/product",productrouter);
-
+app.use('/auth', userrouter);
+app.listen(PORT, async () => {
     try {
         await connectDB(url);
         console.log(`Server is running on port ${PORT}`);
-    }
-    catch(err){
+    } catch (err) {
         console.log(err);
     }
+});
+
+app.use('/auth', userrouter);
+app.use('/product', productrouter);
+app.use('/profile', profileRouter);
+app.use('/order', orderRouter); // Updated to use 'orderRouter'
+
+app.get('/', (req, res) => {
+    res.send('Product Router');
 });
